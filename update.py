25283227a9b7c5a084028729b50cd0262559e278
Nikki1404 <81031507+Nikki1404@users.noreply.github.com>
2026-06-30 16:20:11 +0530

    Update update.py

--- update.py
+++ update.py
@@ -1,6 +1,1 @@
-re_nikitav@EC03-E01-AICOE1:~/nemotron_finetuned$ ls
-01_enter_training_container.sh                      README.md                         ft_models         results_base.jsonl
-02_prepare_baseline_train_eval_inside_container.sh  README_FINE_TUNING_SAME_IMAGE.md  lightning_logs    results_base_full.jsonl
-03_run_finetuned_server.sh                          app                               raw_wavs          results_proper_ft_full.jsonl
-04_run_base_server.sh                               client.py                         requirements.txt  scripts
-Dockerfile                                          data                              results
+cd /home/CORP/re_nikitav/nemotron_finetuned && rsync -avz --progress --exclude 'ft_models/' --exclude 'lightning_logs/' --exclude '__pycache__/' --exclude '*.pyc' ./ <GCP_USER>@<GCP_EXTERNAL_IP>:~/nemotron_finetuned/
