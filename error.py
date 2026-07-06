f1351524a78bb2bec8ddbee071567a494a8927f5
Nikki1404 <81031507+Nikki1404@users.noreply.github.com>
2026-07-06 20:40:17 +0530

    Update error.py

--- error.py
+++ error.py
@@ -1,5 +1,1 @@
-curl -X POST \
-  "https://nemotron-finetuned-150916788856.us-central1.run.app/v1/audio/transcriptions" \
-  -F "file=@audio.wav" \
-  -F "model=nemotron-3.5-asr-streaming-0.6b" \
-  -F "language=auto"
+curl -X POST "https://nemotron-finetuned-150916788856.us-central1.run.app/v1/audio/transcriptions" -F "file=@C:\Users\re_nikitav\Downloads\a.wav" -F "model=nemotron-3.5-asr-streaming-0.6b" -F "language=auto"
