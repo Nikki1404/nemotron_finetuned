d750d33f2ec5f75a3fbf2deae8edf51f682dfde1
Nikki1404 <81031507+Nikki1404@users.noreply.github.com>
2026-07-06 17:21:19 +0530

    Update error.py

--- error.py
+++ error.py
@@ -1,1 +1,4 @@
 https://console.cloud.google.com/artifacts/docker/emr-dgt-autonomous-uctr1-snbx/us-central1/asr-nemotron-finetuned?project=emr-dgt-autonomous-uctr1-snbx
+
+
+docker tag nemotron_finetuned:latest us-central1-docker.pkg.dev/emr-dgt-autonomous-uctr1-snbx/asr-nemotron-finetuned/nemotron_finetuned:v1
