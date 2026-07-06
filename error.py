ae1f4c104865f6a7162607cc08d924eb36434d48
Nikki1404 <81031507+Nikki1404@users.noreply.github.com>
2026-07-06 08:05:08 +0000

    Update error.py

--- error.py
+++ error.py
@@ -1,17 +1,19 @@
-(base) root@EC03-E01-AICOE1:/home/CORP/re_nikitav/nemotron_finetuned# git remote set-url origin https://ucgithub.exlserv
-ice.com/Unified-Cloud-DevOps/bu-digital-cx-speech-asr-realtime-ml.git
-(base) root@EC03-E01-AICOE1:/home/CORP/re_nikitav/nemotron_finetuned# git push origin main:feature/nemotron_finetuned_3.5
-fatal: unable to access 'https://ucgithub.exlservice.com/Unified-Cloud-DevOps/bu-digital-cx-speech-asr-realtime-ml.git/': Proxy CONNECT aborted
-(base) root@EC03-E01-AICOE1:/home/CORP/re_nikitav/nemotron_finetuned# unset http_proxy
-(base) root@EC03-E01-AICOE1:/home/CORP/re_nikitav/nemotron_finetuned# unset https_proxy
-(base) root@EC03-E01-AICOE1:/home/CORP/re_nikitav/nemotron_finetuned# git push origin main:feature/nemotron_finetuned_3.5
+(base) root@EC03-E01-AICOE1:/home/CORP/re_nikitav/nemotron_finetuned# git push -f origin main:feature/nemotron_finetuned_3.5
 Username for 'https://ucgithub.exlservice.com': re-nikitav
 Password for 'https://re-nikitav@ucgithub.exlservice.com':
+Enumerating objects: 544, done.
+Counting objects: 100% (544/544), done.
+Delta compression using up to 8 threads
+Compressing objects: 100% (534/534), done.
+Writing objects: 100% (544/544), 8.95 GiB | 45.63 MiB/s, done.
+Total 544 (delta 68), reused 36 (delta 1), pack-reused 0
+remote: Resolving deltas: 100% (68/68), done.
+remote: error: Please contact CloudInfraSupport@exlservice.com
+remote: error: File ft_models/nemotron_inspira_proper_ft.nemo is 2434.81 MB; this exceeds GitHub Enterprise's file size limit of 1000.00 MB
+remote: error: File ft_models/finetuned_nemotron_final.nemo is 2434.81 MB; this exceeds GitHub Enterprise's file size limit of 1000.00 MB
+remote: error: File ft_models/finetuned_nemotron_v2_lr2e6_ep3.nemo is 2434.81 MB; this exceeds GitHub Enterprise's file size limit of 1000.00 MB
+remote: error: File ft_models/finetuned_nemotron_v3_lr1e6_ep3.nemo is 2434.81 MB; this exceeds GitHub Enterprise's file size limit of 1000.00 MB
+remote: error: GH001: Large files detected. You may want to try Git Large File Storage - https://git-lfs.github.com.
 To https://ucgithub.exlservice.com/Unified-Cloud-DevOps/bu-digital-cx-speech-asr-realtime-ml.git
- ! [rejected]        main -> feature/nemotron_finetuned_3.5 (fetch first)
+ ! [remote rejected] main -> feature/nemotron_finetuned_3.5 (pre-receive hook declined)
 error: failed to push some refs to 'https://ucgithub.exlservice.com/Unified-Cloud-DevOps/bu-digital-cx-speech-asr-realtime-ml.git'
-hint: Updates were rejected because the remote contains work that you do
-hint: not have locally. This is usually caused by another repository pushing
-hint: to the same ref. You may want to first integrate the remote changes
-hint: (e.g., 'git pull ...') before pushing again.
-hint: See the 'Note about fast-forwards' in 'git push --help' for details.
