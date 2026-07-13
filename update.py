c231f218bf9d9a0246219ee5bd5f76f59f3efc35
Nikki1404 <81031507+Nikki1404@users.noreply.github.com>
2026-07-13 15:49:26 +0530

    Update update.py

--- update.py
+++ update.py
@@ -1,5 +1,11 @@
 cd /home/CORP/re_nikitav/nemotron_finetuned && rsync -avz --progress --exclude 'ft_models/' --exclude 'lightning_logs/' --exclude '__pycache__/' --exclude '*.pyc' ./ nikita_verma2@35.223.163.238:~/nemotron_finetuned/
 
+python3.11 -m pip uninstall -y numba llvmlite cuda-python cuda-bindings cuda-core cuda-pathfinder
+python3.11 -m pip install --no-cache-dir "numba==0.60.0" "llvmlite==0.43.0"
+apt update && apt install -y cuda-nvvm-12-4
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+export CUDA_HOME=/usr/local/cuda-12.4
+export LD_LIBRARY_PATH=/usr/local/cuda-12.4/nvvm/lib64:$LD_LIBRARY_PATH
 
 unset NUMBA_CUDA_USE_NVIDIA_BINDING
 mkdir -p /srv/models
